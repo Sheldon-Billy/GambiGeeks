b0f896b41b1c85e462678884ce5c0804780db0de
Sheldon-Billy <sheldonbilly0723@gmail.com>
2025-09-05 10:09:24 +0300

    Ui Polishing

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,25 @@
-
-import PictureZooming from "./Components/PictureZooming"
-import TrustedClients from "./Components/TrustedClients"
+import Home from "./Components/Home";
+import TrustedClients from "./Components/TrustedClients";
 import { useState } from "react";
 import LoadingScreen from "./Components/LoadingScreen";
-
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
 
   return (
     <div>
-
       {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}
 
       <div
-        className={`min-h-screen transition-opacity duration-700 ${isLoaded ? "opacity-100" : "opacity-0"
-          }`}
+        className={`min-h-screen transition-opacity duration-700 ${
+          isLoaded ? "opacity-100" : "opacity-0"
+        }`}
       >
-        <PictureZooming />
+        <Home />
         <TrustedClients />
-
       </div>
-
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
